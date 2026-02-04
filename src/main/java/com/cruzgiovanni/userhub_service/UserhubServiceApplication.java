97142dc507c68212a7c888f94e53286e12ac62d3
Giovanni Cruz <giovannicruzvinhedo@gmail.com>
2026-02-04 20:20:53 -0300

    feat: Initial commit

---
+++ src/main/java/com/cruzgiovanni/userhub_service/UserhubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cruzgiovanni.userhub_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserhubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserhubServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cruzgiovanni/userhub_service/UserhubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cruzgiovanni.userhub_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserhubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
