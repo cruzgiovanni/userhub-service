1ab878a13c42d798d0633ca5a17990958fc91d75
Giovanni Cruz <giovannicruzvinhedo@gmail.com>
2026-02-04 21:36:07 -0300

    feat: Add UserRepository and UserService with CRUD operations

---
+++ src/main/java/com/cruzgiovanni/userhub_service/business/UserService.java
@@ -0,0 +1,41 @@
+package com.cruzgiovanni.userhub_service.business;
+
+import com.cruzgiovanni.userhub_service.infrastructure.entitys.User;
+import com.cruzgiovanni.userhub_service.infrastructure.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void saveUser(User user) {
+        userRepository.saveAndFlush(user);
+    }
+
+    public User getUserById(Integer id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(
+                () -> new RuntimeException("User with email " + email + " not found")
+        );
+    }
+
+    public void updateUserNameById(Integer id, User user) {
+        User userEntity = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User with id " + id + " not found"));
+        User updatedUser = User.builder()
+                .id(userEntity.getId())
+                .email(user.getEmail() != null ? user.getEmail() : userEntity.getEmail())
+                .name(user.getName() != null ? user.getName() : userEntity.getName())
+                .build();
+        userRepository.saveAndFlush(updatedUser);
+    }
+
+    public void deleteUserByEmail(String email) {
+        userRepository.deleteUserByEmail(email);
+    }
+}
