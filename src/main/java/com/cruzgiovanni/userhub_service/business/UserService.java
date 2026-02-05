7e42c61e2dbf37fcdcbd9736e7a10b847c3f0d85
Giovanni Cruz <giovannicruzvinhedo@gmail.com>
2026-02-04 21:45:11 -0300

    refactor: Replace @Autowired with constructor injection using @RequiredArgsConstructor

--- src/main/java/com/cruzgiovanni/userhub_service/business/UserService.java
+++ src/main/java/com/cruzgiovanni/userhub_service/business/UserService.java
@@ -2,14 +2,14 @@ package com.cruzgiovanni.userhub_service.business;
 
 import com.cruzgiovanni.userhub_service.infrastructure.entitys.User;
 import com.cruzgiovanni.userhub_service.infrastructure.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public void saveUser(User user) {
         userRepository.saveAndFlush(user);
@@ -20,18 +20,22 @@ public class UserService {
     }
 
     public User getUserByEmail(String email) {
-        return userRepository.findByEmail(email).orElseThrow(
-                () -> new RuntimeException("User with email " + email + " not found")
-        );
+        return userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new RuntimeException("User with email " + email + " not found"));
     }
 
     public void updateUserNameById(Integer id, User user) {
-        User userEntity = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User with id " + id + " not found"));
+        User userEntity = userRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("User with id " + id + " not found"));
+
         User updatedUser = User.builder()
                 .id(userEntity.getId())
                 .email(user.getEmail() != null ? user.getEmail() : userEntity.getEmail())
                 .name(user.getName() != null ? user.getName() : userEntity.getName())
                 .build();
+
         userRepository.saveAndFlush(updatedUser);
     }
 
