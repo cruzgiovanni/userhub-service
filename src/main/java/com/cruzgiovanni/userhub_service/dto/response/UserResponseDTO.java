9e6858b0c2c019980c7b692d2818c71a2b6e837e
Giovanni Cruz <giovannicruzvinhedo@gmail.com>
2026-02-04 22:11:33 -0300

    feat: create request and response DTO for User controller

---
+++ src/main/java/com/cruzgiovanni/userhub_service/dto/response/UserResponseDTO.java
@@ -0,0 +1,23 @@
+package com.cruzgiovanni.userhub_service.dto.response;
+
+import com.cruzgiovanni.userhub_service.infrastructure.entitys.User;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponseDTO {
+    private Integer id;
+    private String email;
+    private String name;
+
+    public static UserResponseDTO fromEntity(User user) {
+        return UserResponseDTO.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .name(user.getName())
+                .build();
+    }
+}
