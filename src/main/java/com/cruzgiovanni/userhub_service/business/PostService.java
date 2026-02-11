9a19e82a5ca209cb17af93050d27e9bf2a8917ea
Giovanni Cruz <giovannicruzvinhedo@gmail.com>
2026-02-11 20:57:01 -0300

    feat: create posts service

---
+++ src/main/java/com/cruzgiovanni/userhub_service/business/PostService.java
@@ -0,0 +1,32 @@
+package com.cruzgiovanni.userhub_service.business;
+
+import com.cruzgiovanni.userhub_service.infrastructure.entities.Post;
+import com.cruzgiovanni.userhub_service.infrastructure.repositories.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public void createPost(Post post) {
+        postRepository.saveAndFlush(post);
+    }
+
+    public Post getPostById (Integer id) {
+        return postRepository.findById(Long.valueOf(id)).orElseThrow(() ->
+                new RuntimeException("Post not found"));
+    }
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    public void deletePostById(Integer id) {
+        postRepository.deleteById(Long.valueOf(id));
+    }
+
+}
