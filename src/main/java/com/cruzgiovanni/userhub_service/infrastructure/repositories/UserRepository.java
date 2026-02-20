93b75e64b8fa20f4b98d3f78078b768c8880c92c
Giovanni Cruz <giovannicruzvinhedo@gmail.com>
2026-02-20 19:33:43 -0300

    feat: add method to find User by login in UserRepository

--- src/main/java/com/cruzgiovanni/userhub_service/infrastructure/repositories/UserRepository.java
+++ src/main/java/com/cruzgiovanni/userhub_service/infrastructure/repositories/UserRepository.java
@@ -3,10 +3,13 @@ package com.cruzgiovanni.userhub_service.infrastructure.repositories;
 import com.cruzgiovanni.userhub_service.infrastructure.entities.User.User;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    UserDetails findByLogin(String login);
 
     Optional<User> findByEmail(String email);
 
